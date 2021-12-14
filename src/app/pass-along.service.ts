54db805fd9f7a6726873a20f9887ead039cccd4f
991571925 <woloszym@sheridancollege.ca>
2021-12-14 06:11:02 -0500

    Service code added

--- src/app/pass-along.service.ts
+++ src/app/pass-along.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Woloszym } from "./woloszym"
+import { loginName } from "src/assets/data/woloszymData"
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PassAlongService {
 
   constructor() { }
+
+  getWoloszymData(): Woloszym {
+    return loginName;
+  }
 }
