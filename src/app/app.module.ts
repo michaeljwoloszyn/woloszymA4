5a494880ad68537b4c8f6a1d10b684513848dfef
991571925 <woloszym@sheridancollege.ca>
2021-12-14 05:12:21 -0500

    Book component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
