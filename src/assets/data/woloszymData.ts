f53fe2279a839e89424b40b1cf4887c12ec8a495
991571925 <woloszym@sheridancollege.ca>
2021-12-14 07:03:44 -0500

    Added typeScript data code

---
+++ src/assets/data/woloszymData.ts
@@ -0,0 +1,9 @@
+import { Woloszym } from "../../app/woloszym"
+
+export const loginName: Woloszym = {
+  studentNum: 991571925,
+  name: 'Michael Woloszyn',
+  loginName: 'woloszym',
+  campus: 'Davis',
+  assignment: 'Assignment4'
+};
