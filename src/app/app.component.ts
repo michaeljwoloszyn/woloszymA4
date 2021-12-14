869debba35c52bdc487a3a41a883e2f181d329f1
991571925 <woloszym@sheridancollege.ca>
2021-12-14 06:17:12 -0500

    Added app component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PassAlongService } from './pass-along.service'
+import { Woloszym } from './woloszym';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'woloszymA4';
+}
+
+export class HeaderComponent {
+  assignment!: Woloszym;
+  name!: Woloszym;
+
+  constructor(private passService: PassAlongService) { }
+
+  getWoloszymData() {
+    this.assignment = this.passService.getWoloszymData();
+    this.name = this.passService.getWoloszymData();
+  }
+}
+
+export class FooterComponent {
+  name!: Woloszym;
+  studentNum!: Woloszym;
+  loginName!: Woloszym;
+  campus!: Woloszym;
+
+  constructor(private passService: PassAlongService) { }
+
+  getWoloszymData() {
+    this.name = this.passService.getWoloszymData();
+    this.studentNum = this.passService.getWoloszymData();
+    this.loginName = this.passService.getWoloszymData();
+    this.campus = this.passService.getWoloszymData();
+  }
 }
